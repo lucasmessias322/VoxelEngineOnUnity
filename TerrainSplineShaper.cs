a7d63a826b5eb823bfb79d751d037c867fab7a9d
Lucas Messias <lucasmessiaspereira322@gmail.com>
2026-04-01 18:14:54 -0300

    melhorado sistema de splines

--- TerrainSplineShaper.cs
+++ TerrainSplineShaper.cs
@@ -8,19 +8,60 @@ public struct TerrainSplinePoint
     public float location;
     public float value;
     public float derivative;
+    public bool splitTangents;
+    public float derivativeIn;
+    public float derivativeOut;
 
     public TerrainSplinePoint(float location, float value, float derivative = 0f)
     {
         this.location = location;
         this.value = value;
         this.derivative = derivative;
+        splitTangents = false;
+        derivativeIn = derivative;
+        derivativeOut = derivative;
+    }
+
+    public TerrainSplinePoint(float location, float value, float derivativeIn, float derivativeOut)
+    {
+        this.location = location;
+        this.value = value;
+        derivative = (derivativeIn + derivativeOut) * 0.5f;
+        splitTangents = math.abs(derivativeIn - derivativeOut) > 1e-4f;
+        this.derivativeIn = derivativeIn;
+        this.derivativeOut = derivativeOut;
+    }
+
+    public float GetDerivativeIn()
+    {
+        return splitTangents ? derivativeIn : derivative;
+    }
+
+    public float GetDerivativeOut()
+    {
+        return splitTangents ? derivativeOut : derivative;
+    }
+
+    public TerrainSplinePoint Sanitized()
+    {
+        TerrainSplinePoint sanitized = this;
+        float inDerivative = splitTangents ? derivativeIn : derivative;
+        float outDerivative = splitTangents ? derivativeOut : derivative;
+
+        sanitized.derivativeIn = inDerivative;
+        sanitized.derivativeOut = outDerivative;
+        sanitized.derivative = math.abs(inDerivative - outDerivative) <= 1e-4f
+            ? inDerivative
+            : (inDerivative + outDerivative) * 0.5f;
+        sanitized.splitTangents = math.abs(inDerivative - outDerivative) > 1e-4f;
+        return sanitized;
     }
 }
 
 [Serializable]
 public struct TerrainSpline
 {
-    public const int MaxPoints = 8;
+    public const int MaxPoints = 16;
 
     public bool enabled;
     public float smoothing;
@@ -33,6 +74,14 @@ public struct TerrainSpline
     public TerrainSplinePoint point5;
     public TerrainSplinePoint point6;
     public TerrainSplinePoint point7;
+    public TerrainSplinePoint point8;
+    public TerrainSplinePoint point9;
+    public TerrainSplinePoint point10;
+    public TerrainSplinePoint point11;
+    public TerrainSplinePoint point12;
+    public TerrainSplinePoint point13;
+    public TerrainSplinePoint point14;
+    public TerrainSplinePoint point15;
 
     public static TerrainSpline Create(params TerrainSplinePoint[] points)
     {
@@ -61,7 +110,15 @@ public struct TerrainSpline
             case 5: return point5;
             case 6: return point6;
             case 7: return point7;
-            default: return point7;
+            case 8: return point8;
+            case 9: return point9;
+            case 10: return point10;
+            case 11: return point11;
+            case 12: return point12;
+            case 13: return point13;
+            case 14: return point14;
+            case 15: return point15;
+            default: return point15;
         }
     }
 
@@ -77,6 +134,14 @@ public struct TerrainSpline
             case 5: point5 = point; break;
             case 6: point6 = point; break;
             case 7: point7 = point; break;
+            case 8: point8 = point; break;
+            case 9: point9 = point; break;
+            case 10: point10 = point; break;
+            case 11: point11 = point; break;
+            case 12: point12 = point; break;
+            case 13: point13 = point; break;
+            case 14: point14 = point; break;
+            case 15: point15 = point; break;
         }
     }
 
@@ -86,7 +151,36 @@ public struct TerrainSpline
         sanitized.smoothing = math.saturate(smoothing);
         sanitized.pointCount = math.clamp(pointCount, 0, MaxPoints);
         if (sanitized.pointCount <= 0)
+        {
             sanitized.enabled = false;
+            return sanitized;
+        }
+
+        TerrainSplinePoint[] points = new TerrainSplinePoint[sanitized.pointCount];
+        for (int i = 0; i < sanitized.pointCount; i++)
+            points[i] = sanitized.GetPoint(i).Sanitized();
+
+        for (int i = 1; i < points.Length; i++)
+        {
+            TerrainSplinePoint candidate = points[i];
+            int j = i - 1;
+            while (j >= 0 && points[j].location > candidate.location)
+            {
+                points[j + 1] = points[j];
+                j--;
+            }
+
+            points[j + 1] = candidate;
+        }
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            TerrainSplinePoint point = points[i];
+            if (i > 0 && point.location <= points[i - 1].location)
+                point.location = points[i - 1].location + 1e-4f;
+
+            sanitized.SetPoint(i, point);
+        }
 
         return sanitized;
     }
@@ -220,9 +314,9 @@ public static class TerrainSplineEvaluator
         float h11 = ttt - tt;
 
         return h00 * start.value +
-               h10 * delta * start.derivative +
+               h10 * delta * start.GetDerivativeOut() +
                h01 * end.value +
-               h11 * delta * end.derivative;
+               h11 * delta * end.GetDerivativeIn();
     }
 
 }
